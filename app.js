187b9216cf55033499edfbadad86db9cb525eaa5
flbtp <rapportsflbtp@gmail.com>
2026-09-22 23:50:01 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -12,7 +12,7 @@
  * Numéro affiché en bas de l'accueil et de l'écran de connexion.
  * À augmenter à chaque dépôt de nouveaux fichiers sur GitHub : c'est le seul numéro à changer.
  */
-const VERSION_APPLI = '12';
+const VERSION_APPLI = '13';
 
 // ---------------------------------------------------------------------------
 // Petits outils
